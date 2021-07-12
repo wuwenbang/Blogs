0ce9930c81af0a051b8635697b662bd8bf16696d
wuwenbang <wuwenbang@foxmail.com>
2021-07-12 13:11:35 +0800

    update

--- TypeScript/demo.ts
+++ TypeScript/demo.ts
@@ -1,7 +1,1 @@
-// type T = string & unknown;
-// type U = string | unknown;
-
-type T = string & never;
-type U = string | never;
-type Void = void;
-const v: void = undefined;
+type T3 = Extract<string | (() => void), Function>;
