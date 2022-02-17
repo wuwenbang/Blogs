1323334b90957feb3b77285c197b977d3cb3c22b
wuwenbang <wuwenbang@metrodata.cn>
2022-02-17 19:36:47 +0800

    docs

--- Interview/demo.js
+++ Interview/demo.js
@@ -21,8 +21,59 @@ function debounce(fn, wait) {
   };
 }
 
-let f = debounce(() => console.log('hello'), 1000);
-f();
-setTimeout(() => {
-  f();
-}, 500);
+// 发布订阅
+const eventHub = {
+  map: {},
+  on(name, fn) {
+    // 入队
+    eventHub.map[name] = eventHub.map[name] || [];
+    eventHub.map[name].push(fn);
+  },
+  off(name, fn) {
+    const list = eventHub.map[name];
+    if (!list) return;
+    const index = list.indexOf(fn);
+    if (index < 0) return;
+    list.splice(index, 1);
+  },
+  emit(name, data) {
+    const list = eventHub.map[name];
+    if (!list) return;
+    list.map(f => f(data));
+  },
+};
+
+// 手写AJAX
+var request = new XMLHttpRequest();
+
+request.open('GET', '/xxx');
+request.onreadystatechange = () => {
+  if (request.readyState === 4) {
+    if (
+      (request.status >= 200 && request.status < 300) ||
+      request.status === 304
+    ) {
+      console.log('success');
+    } else {
+      console.log('failed');
+    }
+  }
+};
+request.onerror = () => {
+  console.log('error');
+};
+request.send('{"name":"frank"}');
+
+// 手写 Promise
+
+class Promise2{
+  constructor(fn){
+
+  }
+  then(f1,f2){
+
+  }
+  catch(){
+
+  }
+}
